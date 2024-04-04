3798db9e0e19da9a8e6bc360476256e36d9c81ff
Benjamin Chin <lazycatlab@hotmail.com>
2024-04-04 15:21:56 +0800

    Give concrete values to generate_RSA_keys' test case.

--- test_myrsa.c
+++ test_myrsa.c
@@ -12,10 +12,19 @@ int main()
 {
 	/* Test generate_RSA_keys function */
 	{
-		int pub_key[2];
-		int priv_key[2];
-		generate_RSA_keys(11, 17, pub_key, priv_key);
-		assert(pub_key[0] > 0 && priv_key[0] > 0);
+#define PRIME1 11
+#define PRIME2 17
+#define EXPECTED_KEY1 3
+#define EXPECTED_KEY2 107
+#define EXPECTED_MODULUS 187
+
+		int key1[2];
+		int key2[2];
+		generate_RSA_keys(PRIME1, PRIME2, key1, key2);
+		assert((key1[0] == EXPECTED_KEY1) &&
+		       (key1[1] == EXPECTED_MODULUS) &&
+		       (key2[0] == EXPECTED_KEY2) &&
+		       (key2[1] == EXPECTED_MODULUS));
 	}
 
 	/* Add more tests as needed... */
