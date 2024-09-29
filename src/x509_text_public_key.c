f977531664e015b86af14d7de2e48b6b8b43c4c2
Benjamin Chin <lazycatlab@hotmail.com>
2024-09-29 22:00:50 +0800

    Add two OIDs for PKCS #9.

--- src/x509_text_public_key.c
+++ src/x509_text_public_key.c
@@ -201,6 +201,8 @@ enum OID_TYPE {
 	OID_TYPE_RSA_ENCRYPTION,
 	OID_TYPE_SHA1_WITH_RSA_ENCRYPTION,
 	OID_TYPE_EMAIL_ADDRESS,
+	OID_TYPE_UNSTRUCTURED_NAME,
+	OID_TYPE_CHALLENGE_PASSWORD,
 	OID_TYPE_APPLE_SECURITY_86,
 	OID_TYPE_JURISDICTION_OF_INCORPORATION_LOCALITY_NAME,
 	OID_TYPE_JURISDICTION_OF_INCORPORATION_STATE_OR_PROVINCE_NAME,
@@ -253,7 +255,11 @@ OID oid_database[] = {
 	{ 4, { 1, 2, 840, 113549 }, "rsadsi" }, /* X.509 */
 	{ 7, { 1, 2, 840, 113549, 1, 1, 1 }, "rsaEncryption" }, /* RFC 4055 */
 	{ 7, { 1, 2, 840, 113549, 1, 1, 5 }, "sha1WithRSAEncryption" },
-	{ 7, { 1, 2, 840, 113549, 1, 9, 1 }, "emailAddress" }, /* RFC 5280 */
+
+	/* PKCS #9 */
+	{ 7, { 1, 2, 840, 113549, 1, 9, 1 }, "pkcs-9-ub-emailAddress" },
+	{ 7, { 1, 2, 840, 113549, 1, 9, 2 }, "pkcs-9-ub-unstructuredName" },
+	{ 7, { 1, 2, 840, 113549, 1, 9, 7 }, "pkcs-9-at-challengePassword" },
 
 	/* Apple Security */
 	{ 7, { 1, 2, 840, 113635, 100, 6, 86 }, "appleSecurity(6)?(86)" },
@@ -484,6 +490,12 @@ void print_name(FILE *fp, int length)
 			break;
 		case OID_TYPE_EMAIL_ADDRESS:
 			printf("E=");
+			break;
+		case OID_TYPE_UNSTRUCTURED_NAME:
+			printf("UN=");
+			break;
+		case OID_TYPE_CHALLENGE_PASSWORD:
+			printf("CP=");
 			break;
 		case OID_TYPE_BUSINESS_CATEGORY:
 			printf("BC=");
